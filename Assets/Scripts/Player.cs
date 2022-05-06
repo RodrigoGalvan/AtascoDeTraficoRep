bce17731b20b16873e4da0836241035461299af3
RodrigoGalvan <70443221+RodrigoGalvan@users.noreply.github.com>
2022-05-06 14:11:48 -0500

    Movimiento completo de jugador hacia enfrente con vueltas (Cambio de usar raycasts a mover punto por punto para dar vueltas) y sistema de luces con universal pipeline

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,14 +14,8 @@ public class Player : MonoBehaviour
     private bool up; //Vuelta a hacia arriba
     private int xDir; //Dirrecion de movimiento en x
     private int yDir; //Dirrecion de movimiento en y
-    private bool leftDown; //Tile que permite ir hacia la izquierda o abajo
-    private bool rightUp; //Tile que permite ir hacia la derecha o arriba
-    private bool rightDown; //Tile que permite ir hacia la derecha o abajo
-    private bool leftUp; //Tile que permite ir hacia la izquierda o arriba
-    private RaycastHit2D hit; //Objeto de raycast
-    private RaycastHit2D hit2; //Objeto de raycast
-    bool turning; //Si ya dio la vuelta el carro
     public TextMeshProUGUI textOnDisplay;
+     public GameObject movePoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -33,7 +27,6 @@ public class Player : MonoBehaviour
         right = false;
         up = false;
         down = false;
-        turning = false;
 
     }
 
@@ -73,59 +66,80 @@ public class Player : MonoBehaviour
             right = false;
             up = false;
         }
-
+        
         //Si no ha dado la vuelta
-        if (turning == false)
+
+        //Si esta en el tile correcto y si no hay un collider arriba de el
+        if (movePoint.GetComponent<PointMoved>().canTurnLeftUp == true )
         {
-            //Si esta en el tile correcto y si no hay un collider arriba de el
-            if (leftUp == true && isFree())
+            //Si es derecha o arriba
+            if (left)
             {
-                //Si es derecha o arriba
-                if (left)
-                {
-                    turnLeft();
-                }
-                if (up)
-                {
-                    turnUp();
-                }
+                turnLeft();
             }
+            if (up)
+            {
+                turnUp();
+            }
+        }
 
-            if (leftDown == true && isFree())
+        if (movePoint.GetComponent<PointMoved>().canTurnLeftDown == true )
+        {
+            if (left)
             {
-                if (left)
-                {
-                    turnLeft();
-                }
-                if (down)
-                {
-                    turnDown();
-                }
+                turnLeft();
             }
+            if (down)
+            {
+                turnDown();
+            }
+        }
 
-            if (rightUp == true && isFree())
+        if (movePoint.GetComponent<PointMoved>().canTurnRightUp == true )
+        {
+            if (right)
             {
-                if (right)
-                {
-                    turnRight();
-                }
-                if (up)
-                {
-                    turnUp();
-                }
+                turnRight();
             }
+            if (up)
+            {
+                turnUp();
+            }
+        }
 
-            if (rightDown == true && isFree())
+        if (movePoint.GetComponent<PointMoved>().canTurnRightDown == true )
+        {
+            if (right)
             {
-                if (right)
-                {
-                    turnRight();
-                }
-                if (down)
-                {
-                    turnDown();
-                }
+                turnRight();
             }
+            if (down)
+            {
+                turnDown();
+            }
+        }
+        
+
+        if (movePoint.GetComponent<PointMoved>().canTurnDown == true)
+        {
+            turnDown();
+        }
+
+        if (movePoint.GetComponent<PointMoved>().canTurnRight == true)
+        {
+            turnRight();
+
+        }
+
+        if (movePoint.GetComponent<PointMoved>().canTurnLeft == true)
+        {
+            turnLeft();
+
+        }
+
+        if (movePoint.GetComponent<PointMoved>().canTurnUp == true)
+        {
+            turnUp();
         }
 
         //Mueve un espacio y cuando llega al siguiente espacio programa para ir al siguiente
@@ -139,108 +153,48 @@ public class Player : MonoBehaviour
 
     }
 
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        //En que tile esta
-
-        switch (collision.tag) {
-            case "Crossing":
-                turning = true;
-                break;
-            case "LeftUp":
-                leftUp = true;
-                break;
-            case "LeftDown":
-                leftDown = true;
-                break;
-            case "RightUp":
-                rightUp = true;
-                break;
-            case "RightDown":
-                rightDown = true;
-                break;
-        };
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        //De que tile acaba de salir
-        if (collision.tag == "Crossing")
-        {
-            turning = false;
-        }
-        leftDown = false;
-        leftUp = false;
-        rightUp = false;
-        rightDown = false;
-
-    }
-
     private void chageText(string text)
     {
         textOnDisplay.text = text;
     }
 
     private void turnLeft() {
-        turning = true;
-        transform.eulerAngles = new Vector3(0, 0, 0);
-        xDir = -1;
-        yDir = 0;
-        left = false;
+        if (Vector3.Distance(transform.position, movepoint.position) == 0f)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            xDir = -1;
+            yDir = 0;
+            left = false;
+        }
     }
 
     private void turnRight() {
-        turning = true;
-        transform.eulerAngles = new Vector3(0, 0, 180);
-        xDir = 1;
-        yDir = 0;
-        right = false;
+        if (Vector3.Distance(transform.position, movepoint.position) == 0f)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 180);
+            xDir = 1;
+            yDir = 0;
+            right = false;
+        }
     }
 
     private void turnDown() {
-        turning = true;
-        transform.eulerAngles = new Vector3(0, 0, 90);
-        xDir = 0;
-        yDir = -1;
-        down = false;
+        if (Vector3.Distance(transform.position, movepoint.position) == 0f)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 90);
+            xDir = 0;
+            yDir = -1;
+            down = false;
+        }
     }
 
     private void turnUp() {
-        turning = true;
-        transform.eulerAngles = new Vector3(0, 0, 270);
-        xDir = 0;
-        yDir = 1;
-        up = false;
-    }
-
-    private bool isFree()
-    {
-        //Raycast dependiendo de la rotacion del carro
-
-        switch (transform.rotation.z) {
-            case 0:
-                hit = Physics2D.Raycast(transform.position + new Vector3(0, 1f, 0), transform.up, 0.2f);
-                hit2 = Physics2D.Raycast(transform.position + new Vector3(-0.5f, 1f, 0), transform.up, 0.2f);
-                break;
-            case 90:
-                hit = Physics2D.Raycast(transform.position + new Vector3(1, 0f, 0), transform.up, 0.2f);
-                hit2 = Physics2D.Raycast(transform.position + new Vector3(1f, -0.5f, 0), transform.up, 0.2f);
-                break;
-            case 180:
-                hit = Physics2D.Raycast(transform.position + new Vector3(0, -1f, 0), transform.up, 0.2f);
-                hit2 = Physics2D.Raycast(transform.position + new Vector3(0.5f, 1f, 0), transform.up, 0.2f);
-                break;
-            case 270:
-                hit = Physics2D.Raycast(transform.position + new Vector3(-1, 0f, 0), transform.up, 0.2f);
-                hit2 = Physics2D.Raycast(transform.position + new Vector3(-1f, 0.5f, 0), transform.up, 0.2f);
-                break;
-        };
-
-        //Checar si hay collider arriba del carro lo cual significa que no puede dar la vuelta
-        if (hit.collider != null) {
-            return hit.collider.tag == "Crossing" && hit2.collider.tag == "Crossing";
+        if (Vector3.Distance(transform.position, movepoint.position) == 0f)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 270);
+            xDir = 0;
+            yDir = 1;
+            up = false;
         }
-        return hit.collider == null && hit2.collider == null; 
     }
 }
