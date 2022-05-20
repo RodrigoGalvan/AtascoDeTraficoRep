4480fdacd3b5008d5a02d8c4808bc9584436a430
Marcelo Benitez <mbenitezm01@gmail.com>
2022-05-20 18:23:34 -0500

    Spawner de enemigos y animacion del carro al girar

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,215 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Range(1f, 10f)] public int moveSpeed; //Speed para mover el carro
+    public Rigidbody2D rig; //RigidBody del carro
+    public Transform movepoint; //Mover el carro hacia un punto en frente de el para tener un movimiento conforme al grid
+    private bool left; //Vuelta a la izquierda
+    private bool right; //Vuelta a la derecha
+    private bool down; //Vuelta a hacia abajo
+    private bool up; //Vuelta a hacia arriba
+    private bool turning;
+    private Quaternion currentAngle;
+    private int xDir; //Dirrecion de movimiento en x
+    private int yDir; //Dirrecion de movimiento en y
+    public TextMeshProUGUI textOnDisplay;
+     public GameObject movePoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Variables iniciales
+        movepoint.parent = null;
+        xDir = -1;
+        yDir = 0;
+        left = false;
+        right = false;
+        up = false;
+        down = false;
+        turning = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //De acuerdo al input a que dirrecion va
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            chageText("Siguiente Vuelta: Izquierda");
+            left = true;
+            right = false;
+            up = false;
+            down = false;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            chageText("Siguiente Vuelta: Derecha");
+            right = true;
+            up = false;
+            down = false;
+            left = false;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            chageText("Siguiente Vuelta: Arriba");
+            up = true;
+            left = false;
+            down = false;
+            right = false;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            chageText("Siguiente Vuelta: Abajo");
+            down = true;
+            left = false;
+            right = false;
+            up = false;
+        }
+        
+        //Si no ha dado la vuelta
+
+        //Si esta en el tile correcto y si no hay un collider arriba de el
+        if (movePoint.GetComponent<PointMoved>().canTurnLeftUp == true )
+        {
+            //Si es derecha o arriba
+            if (left)
+            {
+                turnLeft();
+            }
+            if (up)
+            {
+                turnUp();
+            }
+        }
+
+        if (movePoint.GetComponent<PointMoved>().canTurnLeftDown == true )
+        {
+            if (left)
+            {
+                turnLeft();
+            }
+            if (down)
+            {
+                turnDown();
+            }
+        }
+
+        if (movePoint.GetComponent<PointMoved>().canTurnRightUp == true )
+        {
+            if (right)
+            {
+                turnRight();
+            }
+            if (up)
+            {
+                turnUp();
+            }
+        }
+
+        if (movePoint.GetComponent<PointMoved>().canTurnRightDown == true )
+        {
+            if (right)
+            {
+                turnRight();
+            }
+            if (down)
+            {
+                turnDown();
+            }
+        }
+        
+
+        if (movePoint.GetComponent<PointMoved>().canTurnDown == true)
+        {
+            turnDown();
+        }
+
+        if (movePoint.GetComponent<PointMoved>().canTurnRight == true)
+        {
+            turnRight();
+
+        }
+
+        if (movePoint.GetComponent<PointMoved>().canTurnLeft == true)
+        {
+            turnLeft();
+
+        }
+
+        if (movePoint.GetComponent<PointMoved>().canTurnUp == true)
+        {
+            turnUp();
+        }
+
+        //Mueve un espacio y cuando llega al siguiente espacio programa para ir al siguiente
+        if (Vector3.Distance(transform.position, movepoint.position) == 0f)
+        {
+            movepoint.position += new Vector3(xDir, yDir, 0f);
+        }
+
+        //Mover hacia el siguiente espacio
+        transform.position = Vector3.MoveTowards(transform.position, movepoint.position, moveSpeed * Time.deltaTime);
+
+        if (turning)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, currentAngle, 0.2f);
+            if (transform.rotation == currentAngle)
+            {
+                turning = false;
+            }
+        }
+    }
+
+    private void chageText(string text)
+    {
+        textOnDisplay.text = text;
+    }
+
+    private void turnLeft() {
+        if (Vector3.Distance(transform.position, movepoint.position) == 0f)
+        {
+            turning = true;
+            currentAngle = Quaternion.Euler(0, 0, 0);
+            xDir = -1;
+            yDir = 0;
+            left = false;
+        }
+    }
+
+    private void turnRight() {
+        if (Vector3.Distance(transform.position, movepoint.position) == 0f)
+        {
+            turning = true;
+            currentAngle = Quaternion.Euler(0, 0, 180);
+            xDir = 1;
+            yDir = 0;
+            right = false;
+        }
+    }
+
+    private void turnDown() {
+        if (Vector3.Distance(transform.position, movepoint.position) == 0f)
+        {
+            turning = true;
+            currentAngle = Quaternion.Euler(0, 0, 90);
+            xDir = 0;
+            yDir = -1;
+            down = false;
+        }
+    }
+
+    private void turnUp() {
+        if (Vector3.Distance(transform.position, movepoint.position) == 0f)
+        {
+            turning = true;
+            currentAngle = Quaternion.Euler(0, 0, 270);
+            xDir = 0;
+            yDir = 1;
+            up = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PointMoved.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointMoved : MonoBehaviour
+{
+
+    public bool canTurn;
+    public bool canTurnRightDown;
+    public bool canTurnRightUp;
+    public bool canTurnLeftDown;
+    public bool canTurnLeftUp;
+    public bool canTurnRight;
+    public bool canTurnDown;
+    public bool canTurnLeft;
+    public bool canTurnUp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        switch (collision.tag)
+        {
+            case "LeftUp":
+                canTurnLeftUp = true;
+                break;
+
+            case "LeftDown":
+                canTurnLeftDown = true;
+                break;
+            case "RightUp":
+                canTurnRightUp = true;
+                break;
+
+            case "RightDown":
+                canTurnRightDown = true;
+                break;
+            case "StraightDown":
+                canTurnDown = true;
+                break;
+
+            case "StraightLeft":
+                canTurnLeft = true;
+                break;
+            case "StraightRight":
+                canTurnRight = true;
+                break;
+
+            case "StraightUp":
+                canTurnUp = true;
+                break;
+        };
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        switch (collision.tag)
+        {
+            case "LeftUp":
+                canTurnLeftUp = false;
+                break;
+
+            case "LeftDown":
+                canTurnLeftDown = false;
+                break;
+            case "RightUp":
+                canTurnRightUp = false;
+                break;
+
+            case "RightDown":
+                canTurnRightDown = false;
+                break;
+            case "StraightDown":
+                canTurnDown = false;
+                break;
+
+            case "StraightLeft":
+                canTurnLeft = false;
+                break;
+            case "StraightRight":
+                canTurnRight = false;
+                break;
+
+            case "StraightUp":
+                canTurnUp = false;
+                break;
+        };
+    }
+
+
+}
